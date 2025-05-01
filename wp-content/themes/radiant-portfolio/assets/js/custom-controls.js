d22f482152ddf7a7e418662ec2f8b88a21fbd0b9
caioptk <caioptk@gmail.com>
2025-05-01 23:08:39 +0100

    Cleaned up test files and added my working version of the template

--- wp-content/themes/radiant-portfolio/assets/js/custom-controls.js
+++
@@ -1,9 +0,0 @@
-(function(api) {
-
-    api.sectionConstructor['radiant-portfolio-upsell'] = api.Section.extend({
-        attachEvents: function() {},
-        isContextuallyActive: function() {
-            return true;
-        }
-    });
-})(wp.customize);
