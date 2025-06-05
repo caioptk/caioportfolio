e4868d54f958e878fe7538dae752c1eeaa08cf69
Caio Petelinkar <58465836+caioptk@users.noreply.github.com>
2025-06-05 21:46:16 +0100

    Update footer and single-page patterns, add new home template

--- wp-config.php
+++ wp-config.php
@@ -91,6 +91,7 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
+define( 'WP_ENVIRONMENT_TYPE', 'local' );
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
