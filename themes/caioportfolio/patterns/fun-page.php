65bcdceea8b29c99a80adba576381b42babc9b51
Caio Petelinkar <58465836+caioptk@users.noreply.github.com>
2025-06-06 19:13:49 +0100

    Restructure: move repo into wp-content

---
+++ themes/caioportfolio/patterns/fun-page.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Fun Page
+ * Slug: caioportfolio/fun-page
+ * Categories: caioportfolio
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ */
+?>
+<!-- wp:pattern {"slug":"caioportfolio/cta"} /-->
+<!-- wp:pattern {"slug":"caioportfolio/services"} /-->
